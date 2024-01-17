5f91b6e1c14d890917b602204f1826e0cfacf244
Benjamin Foley <foleybe@oregonstate.edu>
2024-01-16 20:30:54 -0800

    wrote message for completion

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from Foleybe
+        This app was deployed by GitHub automatically for foleybe
         </p>
         <a
           className="App-link"
